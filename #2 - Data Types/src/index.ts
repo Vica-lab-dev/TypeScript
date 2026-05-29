f98de866526530c10dc248327f75ef0083c160c8
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-05-30 00:51:28 +0200

    Add notes on TypeScript types

---
+++ #2 - Data Types/src/index.ts
@@ -0,0 +1,30 @@
+/**
+ * PRIMITIVE TYPES:
+ * -Number
+ * -String
+ * -Boolean
+ * -Bigint
+ * -Symbol
+ * -Null & unefined
+ *
+ * OBJECT TYPES:
+ * -Objects
+ * -Arrays
+ * -Tuples [string, number]
+ * -Enums
+ *
+ * SPECIAL TYPES:
+ * -Any (iskljucuje proveru)
+ * -Unknown
+ *
+ * FUNCTIONS:
+ * -Void (neka funkcija koja ne vraca nista - nema return)
+ * -Never (Kod koda koji se nece izvrsiti)
+ *
+ * -------------------------------------------------------
+ * -Union Types: string|number
+ * -Intersection types: spajamo tipove u jedan
+ * -Literal types
+ * -Interface
+ */
+
