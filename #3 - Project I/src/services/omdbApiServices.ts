7833358af7b5ad98eeff0380e63e5efb2e76921d
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-06-06 14:31:48 +0200

    refactor: make buildUrl return request URL

--- #3 - Project I/src/services/omdbApiServices.ts
+++ #3 - Project I/src/services/omdbApiServices.ts
@@ -3,13 +3,12 @@ import type {ApiParameterInterface} from "../interfaces/ApiParametersInterface";
 const API_KEY = process.env.API_KEY;
 const API_URL = process.env.API_URL;
 
-export function buildUrl(params: ApiParameterInterface[]) {
+function buildUrl(params: ApiParameterInterface[]): string {
     let searchParams = "";
 
     params.forEach((params) => {
         searchParams += `${params.key}=${params.value}&`;
     });
 
-    const url = API_URL+"?"+searchParams+"apikey="+API_KEY;
-    console.log(url);
+    return API_URL+"?"+searchParams+"apikey="+API_KEY;
 }
