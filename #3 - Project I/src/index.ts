755470f23c155bd3e9a81c0f6fe7b0a56c380add
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-06-06 15:23:26 +0200

    feat: render year options

--- #3 - Project I/src/index.ts
+++ #3 - Project I/src/index.ts
@@ -1,4 +1,7 @@
 import {callOMDBApi} from "./services/omdbApiServices";
+import {generateYears} from "./helpers/yearGeneratorHelper";
+
+const selectMovieYear = document.querySelector("#movieYears")  as HTMLSelectElement;
 
 const response = await callOMDBApi([
     {key: "t", value: "Terminator"},
@@ -6,3 +9,5 @@ const response = await callOMDBApi([
 ]);
 
 console.log(response);
+
+generateYears(1960, selectMovieYear, 2025);
