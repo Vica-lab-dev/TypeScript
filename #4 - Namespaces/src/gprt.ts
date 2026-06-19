6d1d5762c4c9e258c79db8f055baea44e38fe198
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-06-19 11:53:22 +0200

    feat: add generic getFirst function

---
+++ #4 - Namespaces/src/gprt.ts
@@ -0,0 +1,17 @@
+// Generics
+
+function getFirst<T>(items: T[]): T | undefined {
+    return items[0];
+}
+
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(getFirst(numbers));
+
+const names = ["Vica", "Marko"];
+console.log(getFirst(names));
+
+const expenses = [
+    {name: "PC", amount: 2000},
+    {name: "Food", amount: 20},
+];
+console.log(getFirst(expenses));
