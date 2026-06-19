61243606f4e4556e3e65e80169df34109723e77e
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-06-19 19:56:20 +0200

    refactor: require at least one API payload parameter

--- #5 - Project II/src/interfaces/ApiUrlInterface.ts
+++ #5 - Project II/src/interfaces/ApiUrlInterface.ts
@@ -2,6 +2,6 @@ import type {ApiPayloadInterface} from "./ApiPayloadInterface";
 
 export interface ApiUrlInterface {
     endpoint: string,
-    data: ApiPayloadInterface[]
+    data: [ApiPayloadInterface, ...ApiPayloadInterface[]]
 
 }
